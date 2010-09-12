3ce881d53beb94bd25e8f3f7260136f19246ec27
Jake.McArthur@gmail.com <Jake.McArthur@gmail.com@9d0e4ec5-6d14-e29b-3149-cabf4038ef1e>
2010-09-12 18:31:35 +0000

    add links to specification in nav bar and in problem description page. also clean up headers in specification.php

--- branches/issue114/planet_wars/www/specification.php
+++ branches/issue114/planet_wars/www/specification.php
@@ -1,5 +1,7 @@
 <?php include 'header.php'; ?>
 
+<h2>Planet Wars Specification</h2>
+
 <div id="TOC"
 ><ul
   ><li
@@ -73,10 +75,10 @@
     ></ul
   ></div
 >
-<h2 id="overview"
+<h3 id="overview"
 ><a href="#TOC"
   >Overview</a
-  ></h2
+  ></h3
 ><p
 >Planet Wars is a game based on <a href="http://www.galcon.com"
   >Galcon</a
@@ -85,16 +87,16 @@
 >A game of Planet Wars takes place on a map which contains several planets, each of which has some number of ships on it. Each planet may have a different number of ships. The planets may belong to one of three different owners: you, your opponent, or neutral. The game has a certain maximum number of turns. At the time of this writing, the maximum number of turns on the official server is 200, but it is not yet a part of the specification. Provided that neither player performs an invalid action, the player with the most ships at the end of the game wins. The game may also end earlier if one of the players loses all his ships, in which case the player that has ships remaining wins instantly. If both players have the same number of ships when the game ends, it’s a draw.</p
 ><p
 >On each turn, the player may choose to send fleets of ships from any planet he owns to any other planet on the map. He may send as many fleets as he wishes on a single turn as long as he has enough ships to supply them. After sending fleets, each planet owned by a player (not owned by neutral) will increase the forces there according to that planet’s “growth” rate. Different planets have different growth rates. The fleets will then take some number of turns to reach their destination planets, where they will then fight any opposing forces there and, if they win, take ownership of the planet. Fleets cannot be redirected during travel. Players may continue to send more fleets on later turns even while older fleets are in transit.</p
-><h2 id="the-map"
+><h3 id="the-map"
 ><a href="#TOC"
   >The Map</a
-  ></h2
+  ></h3
 ><p
 >Maps have no particular dimensions and are defined completely in terms of the planets and fleets in them.</p
-><h3 id="planets"
+><h4 id="planets"
 ><a href="#TOC"
   >Planets</a
-  ></h3
+  ></h4
 ><p
 >Planet <em
   >positions</em
@@ -123,10 +125,10 @@
 >Each planet is also implicitly assigned an <em
   >ID</em
   >. These are assigned as integers according to the order in which the planets are specified in the map, starting from 0. A planet’s ID will never change throughout the game.</p
-><h3 id="fleets"
+><h4 id="fleets"
 ><a href="#TOC"
   >Fleets</a
-  ></h3
+  ></h4
 ><p
 >The <em
   >owner</em
@@ -145,10 +147,10 @@
   > is given as an integer, representing the total number of turns required to make the trip from source to destination. The <em
   >turns remaining</em
   > is also an integer, representing the number of turns left from the current turn to arrive at the destination. Trip lengths are determined at the time of departure by taking the Euclidean distance to the destination <IMG SRC="eqn008.png" WIDTH=56 HEIGHT=20 STYLE="vertical-align: -5px; margin: 0;"> from the source <IMG SRC="eqn005.png" WIDTH=51 HEIGHT=20 STYLE="vertical-align: -5px; margin: 0;"> and rounding up. That is, <IMG SRC="eqn010.png" WIDTH=196 HEIGHT=23 STYLE="vertical-align: -5px; margin: 0;">.</p
-><h3 id="file-format"
+><h4 id="file-format"
 ><a href="#TOC"
   >File Format</a
-  ></h3
+  ></h4
 ><p
 >The map file format is fairly simple. Each line may be blank, a planet, or a fleet, and each line is separated by Unix style line breaks (LF, not CR or CRLF). The <code
   >#</code
@@ -187,18 +189,18 @@ F 2 28 1 2  8 4     # Player two has sent some ships to take over the neutral pl
 >In the above example, player 1’s planet has ID 0, player 2’s planet has ID 1, and the neutral planet has ID 2. In addition to the forces owned by each of the two players on the planets themselves, the two players each have fleets in transit. Player 1 has a fleet of 15 ships that is about to arrive at player 2’s home planet. Player 2 has a fleet of 28 ships that is half way to the neutral planet.</p
 ><p
 >Map files should not contain fleet lines, but this format is also used for game state updates, as will be described below, so the fleet lines are included in the specification here.</p
-><h3 id="about-the-current-maps"
+><h4 id="about-the-current-maps"
 ><a href="#TOC"
   >About the Current Maps</a
-  ></h3
+  ></h4
 ><p
 >Most maps on the server and in the starter packs were generated programmatically by a <a href="http://code.google.com/p/ai-contest/source/browse/trunk/planet_wars/backend/map_generator.py"
   >Python script</a
   >. You may also create your own to use for testing.</p
-><h2 id="turns"
+><h3 id="turns"
 ><a href="#TOC"
   >Turns</a
-  ></h2
+  ></h3
 ><p
 >The game engine performs the following steps repeatedly:</p
 ><ol style="list-style-type: decimal;"
@@ -217,10 +219,10 @@ F 2 28 1 2  8 4     # Player two has sent some ships to take over the neutral pl
 >A <em
   >turn</em
   > is defined as the above four steps. They are performed up to 200 times and then the game stops. This means that the players receive the game state up to 200 times and send sets of orders up to 200 times.</p
-><h3 id="bot-io"
+><h4 id="bot-io"
 ><a href="#TOC"
   >Bot I/O</a
-  ></h3
+  ></h4
 ><p
 >The engine on the official server launches your bot in a sandbox environment and communicates with it via stdin and stdout. It silently absorbs your stderr stream, and your bot is prohibited from writing to files. A single game is a single instance of your bot process. That is, the same bot process is used from turn to turn in a single game, but an entirely different process is used for a new game. The unofficial engines may have their own ways of handling these things, so if you are testing with a different engine you are responsible for being aware of these details on a per case basis.</p
 ><p
@@ -244,10 +246,10 @@ go
 >It’s unlikely that the engine will include comments in this output, but I wouldn’t rely on it not to. Note also that because planet IDs never change, the planets in the bot input will be in the same order from turn to turn.</p
 ><p
 >Once the engine starts sending the game state to the bots, the bots each have 1 second of wall-clock time to receive the game state, process it, and send their orders. (The first turn is an exception, in which case the bots have 3 seconds of wall-clock time. This is to help bots written in languages whose VMs need some “warm-up” time.) Both bots perform these operations concurrently, and they are each unaware of what the other bot is doing. The choice of using wall-clock time rather than CPU time is primarily due to technical difficulties in measuring child processes’ CPU times in real time. If a simple way to monitor CPU time instead is discovered, this part of the spec may change.</p
-><h4 id="bot-orders"
+><h5 id="bot-orders"
 ><a href="#TOC"
   >Bot Orders</a
-  ></h4
+  ></h5
 ><p
 >An “order” is a line of text that the bot sends to the engine to make a fleet depart from a planet on the next game state update. The format of this line is as follows:</p
 ><pre
@@ -270,30 +272,30 @@ go
   ></pre
 ><p
 >That means the bot wants to send 50 ships from planet 1 to planet 17 and 50 ships from planet 4 to planet 17.</p
-><h3 id="game-state-update"
+><h4 id="game-state-update"
 ><a href="#TOC"
   >Game State Update</a
-  ></h3
+  ></h4
 ><p
 >After receiving complete lists of commands from the players, the engine then updates the game state, advancing the game to the next turn. This happens in three phases: departure, advancement, and arrival.</p
-><h4 id="departure"
+><h5 id="departure"
 ><a href="#TOC"
   >Departure</a
-  ></h4
+  ></h5
 ><p
 >In this phase, the players’ commands are carried out. This consists of creating new fleets and removing the appropriate numbers of ships from each planet. Fleet trip lengths are determined by taking the Euclidean distance to the destination from the source and rounding up.</p
-><h4 id="advancement"
+><h5 id="advancement"
 ><a href="#TOC"
   >Advancement</a
-  ></h4
+  ></h5
 ><p
 >This phase advances fleets and grows populations. Fleets are advanced by simply decrementing their “turns remaining” values.</p
 ><p
 >For each planet, if it is owned by a player, its growth rate is added to its number of ships. For example, if the planet is owned by player 1, has 4 ships, and has a growth rate of 2, the planet will next have 6 ships, still having its growth rate of 2 and still being owned by player 1. However, had the planet been owned by neutral instead, its population would not have grown.</p
-><h4 id="arrival"
+><h5 id="arrival"
 ><a href="#TOC"
   >Arrival</a
-  ></h4
+  ></h5
 ><p
 >This phase handles fleets whose “turns remaining” became zero during the advancement phase. It does so by considering each destination planet at a time.</p
 ><p
@@ -324,10 +326,10 @@ go
 >If we apply the original procedure to the above battle, we use the three-way rules. The two largest forces are player 1’s and player 2’s. The winner must be player 1 because he has the most ships, and we subtract player 2’s ships from player 1’s to obtain the new ship count, <IMG SRC="eqn007.png" WIDTH=68 HEIGHT=16 STYLE="vertical-align: -2px; margin: 0;">.</p
 ><p
 >In actuality, all three cases for one, two and three forces follow the same procedure. In all cases, the largest force wins, and the second largest force is subtracted from the first, and in the case of a tie the original owner keeps the planet with zero ships remaining. A critical detail is that if forces completely cancel out then the original owner retains the planet.</p
-><h2 id="endgame-conditions"
+><h3 id="endgame-conditions"
 ><a href="#TOC"
   >Endgame Conditions</a
-  ></h2
+  ></h3
 ><p
 >The following conditions will cause the game to end:</p
 ><ul
@@ -350,10 +352,10 @@ go
   ><li
   >A bot attempts to do something that the tournament manager deems a security issue and is disqualified.</li
   ></ul
-><h2 id="more-information"
+><h3 id="more-information"
 ><a href="#TOC"
   >More Information</a
-  ></h2
+  ></h3
 ><p
 >If you have a question about the game mechanics, you may ask in the <a href="http://ai-contest.com/forum/"
   >forums</a
