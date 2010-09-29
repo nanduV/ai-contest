7ce10e765fa9fd08d265cc8cdffbd128ac2f9d7c
janzert <janzert@9d0e4ec5-6d14-e29b-3149-cabf4038ef1e>
2010-09-29 05:51:59 +0000

    Create branch to work on issue 74, report turns left to bots.

---
+++ branches/issue74-report_turns_left/planet_wars/www/starter_packages.php
@@ -0,0 +1,65 @@
+<?php include 'header.php'; ?>
+
+<h2>Starter Packages</h2>
+<p>Here are the starter packages that we currently offer. Each starter
+  package is a ZIP file which contains:</p>
+<ul>
+  <li>A simple already-working entry to use as a starting point</li>
+  <li>Tools that allow you to run your bot and watch it play graphically</li>
+  <li>Some sample opponents that you can test your bot against</li>
+  <li>One hundred randomly generated maps that you can use for testing</li>
+</ul>
+<p>Since each starter package contains a simple already-working entry, you
+  can actually submit the ZIP file on the Upload Your Code page. See the
+  <a href="quickstart.php">Five Minute Quickstart Guide</a> to learn how to
+  get your name up on the global leaderboard in less than five minutes.</p>
+
+<h3><a href="starter_packages/cpp_starter_package.zip">C++</a></h3>
+<p>Download the C++ starter package to get started writing your entry in
+  C++. The starter package is a simple working entry that you can use as a
+  starting point. You can even submit the zip file on the Upload Your Code
+  page, since it is a valid working entry!
+</p>
+
+<h3><a href="starter_packages/java_starter_package.zip">Java</a></h3>
+<p>Download the Java starter package to get started writing your entry in
+  Java. The starter package is a simple working entry that you can use as a
+  starting point. You can even submit the zip file on the Upload Your Code
+  page, since it is a valid working entry!</p>
+
+<h3><a href="starter_packages/python_starter_package.zip">Python</a></h3>
+<p>Download the Python starter package to get started writing your entry in
+  Python. The starter package is a simple working entry that you can use as a
+  starting point. You can even submit the zip file on the Upload Your Code
+  page, since it is a valid working entry!</p>
+
+<h3><a href="starter_packages/csharp_starter_package.zip">C&#35;</a></h3>
+<p>Download the C&#35; starter package to get started writing your entry in
+  C&#35;. The starter package is a simple working entry that you can use as a
+  starting point. You can even submit the zip file on the Upload Your Code
+  page, since it is a valid working entry!</p>
+  
+
+<h3>Haskell</h3>
+<p>Haskel is supported. We are using GHC version 6.8.2. Check the forums for user contributed starter packs.</p>
+
+<h3>Ruby</h3>
+<p>Ruby is supported. We are using Ruby 1.9.0. Check the forums for user contributed starter packs.</p>
+
+<h3>Javascript</h3>
+<p>Javascript is supported. We are using node v0.2.1. Check the forums for user contributed starter packs.</p>
+
+<h3>PHP</h3>
+<p>PHP is supported. We are using PHP 5.2.4. Check the forums for user contributed starter packs.</p>
+
+<h3>Perl</h3>
+<p>Perl is supported. We are using perl 5.8.8. Check the forums for user contributed starter packs.</p>
+
+<h3>OCaml</h3>
+<p>OCaml is supported. We are using OCaml version 3.10.0. Check the forums for user contributed starter packs.</p>
+
+<h3>Lisp</h3>
+<p>Lisp is supported. We are using SBCL 1.0.11. Check the forums for user contributed starter packs.</p>
+
+
+<?php include 'footer.php'; ?>
