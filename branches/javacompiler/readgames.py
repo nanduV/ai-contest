cdaa74ab87484ae0d2517b24b3ca37e41ca41a5a
jokeserver <jokeserver@9d0e4ec5-6d14-e29b-3149-cabf4038ef1e>
2010-11-25 01:52:40 +0000

    Branching for Java Compiler package support.

---
+++ branches/javacompiler/readgames.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import os
+import tempfile
+import time
+
+import MySQLdb
+from server_info import server_info
+
+# Connect to the database.
+connection = MySQLdb.connect(host = server_info["db_host"],
+                             user = server_info["db_username"],
+                             passwd = server_info["db_password"],
+                             db = server_info["db_name"])
+cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+cursor.execute("""
+  SELECT * FROM submissions s JOIN users u ON s.user_id=u.user_id
+  WHERE status = 40 AND latest = 1
+""")
+
+players = {}
+for row in cursor:
+    s_id = row['submission_id']
+    players[s_id] = "%s,%s,%s" % (row['user_id'], row['username'], s_id)
+
+t = ','.join(str(k) for k in players.keys())
+cursor.execute("""
+  SELECT game_id, winner, loser, map_id, draw, timestamp, player_one, player_two
+  FROM games
+  WHERE player_one IN (%s) AND player_two IN (%s)
+  AND timestamp >= timestampadd(DAY, -10, current_timestamp)""" % (t, t))
+
+pgn_fd, tmp_name = tempfile.mkstemp(dir=".")
+pgn = os.fdopen(pgn_fd, 'w')
+
+for row in cursor:
+    pgn.write('[White "%s"]\n' % (players[row['player_one']],))
+    pgn.write('[Black "%s"]\n' % (players[row['player_two']],))
+    pgn.write('[GameID "%s"]\n' % (row['game_id'],))
+    if row['draw']:
+        res = '1/2-1/2'
+    elif row['winner'] == row['player_one']:
+        res = '1-0'
+    elif row['winner'] == row['player_two']:
+        res = '0-1'
+    else:
+        raise ValueError("Winner is not player 1 or 2 game_id "+ row['game_id'])
+    pgn.write('[Result "%s"]\n%s\n\n' % (res, res))
+
+pgn.close()
+pgn_name = "games-%s.pgn" % (time.strftime("%Y-%m-%d-%H%M"))
+os.rename(tmp_name, pgn_name)
+os.chmod(pgn_name, 0644)
+
+cursor.close()
+connection.close()
+
+print "readpgn", pgn_name
+print "elo"
+print " mm 0 1"
+print " ratings >ratings.txt"
+print " x"
+print "x"
+
